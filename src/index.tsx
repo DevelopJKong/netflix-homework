102535351dc17db8563bc207fc929c88ec065651
DevelopJKong <ybaram40@gmail.com>
2022-04-18 15:15:49 +0900

    material-icon / core 로 인한 에러 해결

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -6,6 +5,7 @@ import App from "./App";
 import { createGlobalStyle } from "styled-components";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { theme } from "./theme";
+import { RecoilRoot } from "recoil";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
